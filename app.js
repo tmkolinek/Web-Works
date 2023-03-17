05e317925bce632747cc88c95e842195051fb01c
Thomas Kolinek <kolinektommy@gmail.com>
2023-03-17 16:18:01 -0700

    finished

---
+++ app.js
@@ -0,0 +1,30 @@
+// 1.
+// http is hyper text transfer protocol that helps servers communicate with each other
+
+// 2. 
+// an address for a internet source
+
+// 3.
+// Domain Name system which can translate website names to actual IP addresses
+
+// 4.
+// helps you pass key value pairs
+
+// 5. 
+// get lets you receive content and post allows you to change content in the server
+
+// 6.
+// request to server in the form of http protocol
+
+// 7.
+// response from server in the form of http protocol
+
+// 8.
+// Headers provide additional information about the request or the response. 
+
+// 9. 
+// Your browser “resolves” the name into an IP address using DNS
+// Your browser makes a request to that IP address, including headers (info about browser, any previous cookies, and other things)
+// The server sends a response (typically, HTML, with a status code (200 if it was sucessful)
+// The browser makes a DOM from that HTML, and finds any other resources needed (images, CSS, JavaScript, etc)
+// The browser makes separate HTTP requests for those resources and receives response from the server for each
